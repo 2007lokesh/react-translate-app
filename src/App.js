56cd9e05e5dd7751f890c549169b3cfe52b7dd8b
2007lokesh <130909953+2007lokesh@users.noreply.github.com>
2023-09-26 01:19:00 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,90 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
+import axios from "axios";
+
+// const axios = require('axios').default
 
 function App() {
+  const [option, setOption] = useState([]);
+  const [to, setTO] = useState("en");
+  const [from, setFrom] = useState("en");
+  const [input, setInput] = useState("");
+  const [output, setOutput] = useState("");
+
+  const translate = () => {
+    const params = new URLSearchParams();
+    params.append("q", input);
+    params.append("source", from);
+    params.append("target", to);
+    params.append("api_key", "xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx");
+
+    axios
+      .post(
+        "https://libretranslate.de/translate",params,
+        {
+          q: input,
+          source: from,
+          target: to,
+          api_key: "xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx",
+        },
+        {
+          headers: {
+            accept: "application/json",
+            "Content Type": "application/x-www-form-urlencoded",
+          },
+        }
+      )
+      .then((res) => {
+        console.log(res.data);
+        setOutput(res.data.translatedText)
+      });
+  };
+
+  useEffect(() => {
+    axios
+      .get("https://libretranslate.de/languages", {
+        headers: { accept: "application/json" },
+      })
+      .then((res) => {
+        console.log(res.data);
+        setOption(res.data);
+      });
+  }, []);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        From ({from}):
+        <select onChange={(e) => setFrom(e.target.value)}>
+          {option.map((opt) => (
+            <option key={opt.code} value={opt.code}>
+              {opt.name}
+            </option>
+          ))}
+        </select>
+        To ({to}):
+        <select onChange={(e) => setTO(e.target.value)}>
+          {option.map((opt) => (
+            <option key={opt.code} value={opt.code}>
+              {opt.name}
+            </option>
+          ))}
+        </select>
+        <div>
+          <textarea
+            name=""
+            id=""
+            cols="80"
+            rows="15"
+            onInput={(e) => setInput(e.target.value)}
+          ></textarea>
+        </div>
+        <div>
+          <textarea name="" id="" cols="80" rows="15" value={output}></textarea>
+        </div>
+        <div>
+          <button onClick={e=>translate()}>Translate</button>
+        </div>
+      </div>
     </div>
   );
 }
